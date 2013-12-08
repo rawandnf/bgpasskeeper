312f64834f4e99672b65141045014d488fcd4094
Rawand Fatih <rawandnf@gmail.com>
2013-12-08 22:55:40 +0300

    Updates, Adding the menu bar and initializing it.

--- BG PassKeeper/src/com/brwskagroup/bgpasskeeper/MainFrame.java
+++ BG PassKeeper/src/com/brwskagroup/bgpasskeeper/MainFrame.java
@@ -24,6 +24,10 @@
 package com.brwskagroup.bgpasskeeper;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JSeparator;
 
 
 
@@ -33,5 +37,77 @@ import javax.swing.JFrame;
  * @author Brwska Group (Rawand Fatih)
  */
 public class MainFrame extends JFrame {
+
+  private JMenuBar menuBar;
+  private JMenu fileMenu;
+  private JMenuItem newMenuItem;
+  private JMenuItem openMenuItem;
+  private JMenuItem saveMenuItem;
+  private JMenuItem quitMenuItem;
+
+
+  public MainFrame() {
+    super(resources.Strings.APP_NAME);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setSize(500, 500);
+    init();
+    this.setLocationRelativeTo(null);
+    this.setVisible(true);
+  }
+
+
+  /**
+   * Initializes the application.
+   */
+  private void init() {
+    setUpMenuBar();
+  }
+
+
+  /**
+   * Sets up the MenuBar. Adds the items and assign the actions.
+   */
+  private void setUpMenuBar() {
+    initMenuBarItems();
+    setUpFileMenu();
+    setUpEditMenu();
+    setJMenuBar(menuBar);
+  }
+
+
+  /**
+   * Initializes MenuBar object items.
+   */
+  private void initMenuBarItems() {
+    menuBar = new JMenuBar();
+    fileMenu = new JMenu(resources.Strings.FILE_MENU);
+    newMenuItem = new JMenuItem(resources.Strings.NEW_FILE_ITEM);
+    openMenuItem = new JMenuItem(resources.Strings.OPEN_FILE_ITEM);
+    saveMenuItem = new JMenuItem(resources.Strings.SAVE_FILE_ITEM);
+    quitMenuItem = new JMenuItem(resources.Strings.QUIT_FILE_ITEM);
+  }
+
+
+  /**
+   * Sets up the file menu, creates the file, its items and adds it to the
+   * MenuBar with the actions.
+   */
+  private void setUpFileMenu() {
+    fileMenu.add(newMenuItem);
+    fileMenu.add(openMenuItem);
+    fileMenu.add(saveMenuItem);
+    fileMenu.add(new JSeparator());
+    fileMenu.add(quitMenuItem);
+    menuBar.add(fileMenu);
+  }
+
+
+  /**
+   * Sets up the edit menu, creates the edit, its items and adds it to the
+   * MenuBar with the actions.
+   */
+  private void setUpEditMenu() {
+
+  }
 
 }
